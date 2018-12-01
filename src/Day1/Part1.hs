7e1c5540a4d10cf1d9021442507e9f979843ad2b
Martin Øinæs Myrseth <myrseth@gmail.com>
2018-12-01 21:42:42 +0100

    day1: Part 2

--- src/Day1/Part1.hs
+++ src/Day1/Part1.hs
@@ -1,5 +1,6 @@
 module Day1.Part1 where
 
+import Data.IntSet
 import Data.Maybe
 import Text.Read
 
@@ -8,7 +9,19 @@ parseShift ('+' : num) = readMaybe num
 parseShift ('-' : num) = negate <$> readMaybe num
 parseShift _           = Nothing
 
+changeList :: IO [Int]
+changeList = do
+  input <- readFile "./src/Day1/Part1.input"
+  return $ mapMaybe parseShift $ lines input
+
 part1 :: IO ()
 part1 = do
-  ls <- sum . mapMaybe parseShift . lines <$> readFile "./src/Day1/Part1.input"
+  ls <- sum <$> changeList
   print ls
+
+part2 :: IO ()
+part2 = do
+  freqs <- scanl (+) 0 . cycle <$> changeList
+  print $ go empty freqs
+  where go set (f:fs) | f `member` set = f
+                      | otherwise = go (f `insert` set) fs
