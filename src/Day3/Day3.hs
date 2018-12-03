78938397bb525fef8c68eac428ca62621c5095aa
Martin Øinæs Myrseth <myrseth@gmail.com>
2018-12-03 23:52:59 +0100

    day3: Part 2

--- src/Day3/Day3.hs
+++ src/Day3/Day3.hs
@@ -4,21 +4,17 @@ module Day3.Day3 where
 
 import           Control.Monad
 import           Data.Char (isDigit)
+import qualified Data.IntSet as I
 import           Data.List (find)
 import           Data.Vector (Vector, (++))
 import qualified Data.Vector as V
 import           Prelude hiding ((++))
 import           Text.ParserCombinators.ReadP
 
-data Claim = Claim { _id :: Int
-                   , _left :: Int
-                   , _top :: Int
-                   , _width :: Int
-                   , _height :: Int
-                   } deriving Show
-
+type Pos = (Int, Int) -- ^ (x, y)
 type Dim = (Int, Int) -- ^ (w, h)
-data Fabric = Fabric Dim (Vector Int)
+data Fabric = Fabric { _dim :: Dim, _vect :: Vector Int }
+data Claim = Claim {_id :: Int, _idxs :: I.IntSet }
 
 empty :: Fabric
 empty = Fabric (w, h) $ V.replicate (w * h) 0
@@ -28,32 +24,36 @@ parseInt :: ReadP Int
 parseInt = read <$> many1 (satisfy isDigit)
 
 parseClaim :: ReadP Claim
-parseClaim = Claim
-  <$> (char '#' >> parseInt)
-  <*> (string " @ " >> parseInt)
-  <*> (char ',' >> parseInt)
-  <*> (string ": " >> parseInt)
-  <*> (char 'x' >> parseInt <* eof)
+parseClaim = do
+  id <- char '#' >> parseInt
+  pos <- (,) <$> (string " @ " >> parseInt) <*> (char ',' >> parseInt)
+  dim <- (,) <$> (string ": " >> parseInt) <*> (char 'x' >> parseInt <* eof)
+  return $ Claim id (claimIdxs pos dim)
+
+claimIdxs :: Pos -> Dim -> I.IntSet
+claimIdxs (x, y) (w, h) = I.fromList $ do
+  column <- [x .. x + w - 1]
+  row    <- [y .. y + h - 1]
+  return $ index (column, row)
+  where index (x, y) = x + y * (fst . _dim $ empty)
 
 parseClaims :: [String] -> [Claim]
 parseClaims = map fst . concatMap (readP_to_S parseClaim)
 
 mergeClaim :: Fabric -> Claim -> Fabric
-mergeClaim (Fabric (w, h) v) c = Fabric (w, h) v'
-  where v' = V.accum (+) v (map (, 1) $ claimIdxs c)
-        claimIdxs (Claim _ x y w h) = do
-          column <- [x .. x + w - 1]
-          row    <- [y .. y + h - 1]
-          return $ index (column, row)
-        index (x, y) = x + y * w
+mergeClaim (Fabric d v) (Claim _ idxs) = Fabric d v'
+  where v' = V.accum (+) v (map (, 1) (I.toList idxs))
 
 puzzle :: IO ()
 puzzle = do
   claims <- parseClaims . lines <$> readFile "./src/Day3/input.txt"
-
   let (Fabric _ merged) = foldr (flip mergeClaim) empty claims
       part1 = show (V.length $ V.filter (> 1) merged)
   putStrLn $ "part1: " <> part1
 
-  -- let single = V.map (\x -> if x == 1 then 1 else 0) merged
-  -- putStrLn $ "part2: "
+  let indexed = V.filter ((== 1) . snd) $ V.indexed merged
+      indexes = I.fromList $ V.toList $ V.map fst indexed
+
+  case find (\(Claim _ idxs) -> idxs `I.isSubsetOf` indexes) claims of
+    Nothing -> putStrLn "sorry, no matches"
+    Just (Claim id _) -> putStrLn $ "part2: " <> show id
