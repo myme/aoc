82f6bb7e1f784c51ec3c6868bb6ce9992d09451d
Martin Øinæs Myrseth <myrseth@gmail.com>
2018-12-04 22:57:07 +0100

    Automate listing

--- src/Main.hs
+++ src/Main.hs
@@ -6,15 +6,13 @@ import qualified Day3.Day3 as Day3
 import qualified Day4.Day4 as Day4
 
 main :: IO ()
-main = do
-  putStrLn "Day 1"
-  Day1.puzzle
-
-  putStrLn "Day 2"
-  Day2.puzzle
-
-  putStrLn "Day 3"
-  Day3.puzzle
-
-  putStrLn "Day 4"
-  Day4.puzzle
+main = mapM_ printPuzzle $ zip [1 ..]
+  [ Day1.puzzle
+  , Day2.puzzle
+  , Day3.puzzle
+  , Day4.puzzle
+  ]
+  where
+    printPuzzle x = do
+      putStrLn $ show (fst x) <> "."
+      snd x
