759b6f3f5da87448dffdb642aff0a3bdf60376b0
Martin Øinæs Myrseth <myrseth@gmail.com>
2018-12-04 08:51:56 +0100

    Use Data.Ix for indexing

--- src/Day3/Day3.hs
+++ src/Day3/Day3.hs
@@ -5,6 +5,7 @@ module Day3.Day3 where
 import           Control.Monad
 import           Data.Char (isDigit)
 import qualified Data.IntSet as I
+import           Data.Ix (index, range)
 import           Data.List (find)
 import           Data.Vector (Vector, (++))
 import qualified Data.Vector as V
@@ -14,7 +15,7 @@ import           Text.ParserCombinators.ReadP
 type Pos = (Int, Int) -- ^ (x, y)
 type Dim = (Int, Int) -- ^ (w, h)
 data Fabric = Fabric { _dim :: Dim, _vect :: Vector Int }
-data Claim = Claim {_id :: Int, _idxs :: I.IntSet }
+data Claim = Claim {_id :: Int, _idxs :: [Pos] }
 
 empty :: Fabric
 empty = Fabric (w, h) $ V.replicate (w * h) 0
@@ -26,35 +27,31 @@ parseInt = read <$> many1 (satisfy isDigit)
 parseClaim :: ReadP Claim
 parseClaim = do
   id <- char '#' >> parseInt
-  pos <- (,) <$> (string " @ " >> parseInt) <*> (char ',' >> parseInt)
-  dim <- (,) <$> (string ": " >> parseInt) <*> (char 'x' >> parseInt <* eof)
-  return $ Claim id (claimIdxs pos dim)
-
-claimIdxs :: Pos -> Dim -> I.IntSet
-claimIdxs (x, y) (w, h) = I.fromList $ do
-  column <- [x .. x + w - 1]
-  row    <- [y .. y + h - 1]
-  return $ index (column, row)
-  where index (x, y) = x + y * (fst . _dim $ empty)
+  x <- string " @ " >> parseInt
+  y <- char ',' >> parseInt
+  w <- string ": " >> parseInt
+  h <- char 'x' >> parseInt <* eof
+  return $ Claim id (range ((x, y), (x + w - 1, y + h -1)))
 
 parseClaims :: [String] -> [Claim]
 parseClaims = map fst . concatMap (readP_to_S parseClaim)
 
 mergeClaim :: Fabric -> Claim -> Fabric
 mergeClaim (Fabric d v) (Claim _ idxs) = Fabric d v'
-  where v' = V.accum (+) v (map (, 1) (I.toList idxs))
+  where v' = V.accum (+) v (map ((, 1) . index ((0, 0), d)) idxs)
 
 puzzle :: IO ()
 puzzle = do
   claims <- parseClaims . lines <$> readFile "./src/Day3/input.txt"
 
-  let (Fabric _ merged) = foldr (flip mergeClaim) empty claims
+  let (Fabric dim merged) = foldr (flip mergeClaim) empty claims
       part1 = show (V.length $ V.filter (> 1) merged)
 
   putStrLn $ "part1: " <> part1
 
   let indexed = V.filter ((== 1) . snd) $ V.indexed merged
       indexes = I.fromList $ V.toList $ V.map fst indexed
-      part2 = find (\(Claim _ idxs) -> idxs `I.isSubsetOf` indexes) claims
+      pred (Claim _ idxs) = I.fromList (map (index ((0, 0), dim)) idxs) `I.isSubsetOf` indexes
+      part2 = find pred claims
 
   putStrLn $ "part2: " <> maybe "sorry, no matches" (show . _id) part2
