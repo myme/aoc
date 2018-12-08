c39a471bbde45e5812ec68f9ae101132d90270f3
Martin Øinæs Myrseth <myrseth@gmail.com>
2018-12-09 00:19:16 +0100

    day8: Part 2

--- src/Day8/Day8.hs
+++ src/Day8/Day8.hs
@@ -4,23 +4,37 @@ module Day8.Day8 where
 import Text.Read (readMaybe)
 import Utils
 
-getMeta :: [Int] -> [Int]
-getMeta = fst . go 1 []
-  where go _ meta [] = (meta, [])
-        go 0 meta rest = (meta, rest)
-        go n meta (nchild : nmeta : rest) = let
-          (childMeta, rest') = go nchild [] rest
-          (ownMeta, rest'') = splitAt nmeta rest'
-          in go (n - 1) (meta <> ownMeta <> childMeta) rest''
-        go _ _ _ = undefined
+newtype Node = N ([Node], [Int]) -- ^ (children, metadata)
+  deriving Show
 
+parseNodes :: [Int] -> Node
+parseNodes = fst . go
+  where go (nchld : nmeta : rest) = let
+          (children, rest') = foldr parseChild ([], rest) (replicate nchld go)
+          (meta, rest'') = splitAt nmeta rest'
+          in (N (reverse children, meta), rest'')
+        go _ = undefined
+        parseChild f (children, rest) = let
+          (child, rest') = f rest
+          in (child:children, rest')
+
+getMeta :: Node -> [Int]
+getMeta (N (children, meta)) = meta <> concatMap getMeta children
+
+nodeValue :: Node -> Int
+nodeValue (N ([], meta)) = sum meta
+nodeValue (N (children, meta))= sum $ map childSum meta
+  where childSum childIdx = let
+          value = case  drop (childIdx - 1) children of
+                [] -> 0
+                (child:_) -> nodeValue child
+          in value
 
 puzzle :: IO ()
 puzzle =
   traverse readMaybe . words <$> readFile "./src/Day8/input.txt" >>= \case
-
     Nothing -> fail "No parse"
     Just input -> do
-      let part1 = sum $ getMeta input
-      expect "part 1: " 138 part1
-      expect "part 2: " "not implemented!" ""
+      let tree = parseNodes input
+      expect "part 1: " 44338 (sum $ getMeta tree)
+      expect "part 2: " 37560 (nodeValue tree)
