17445f42de7d9ba4d02b7f5abe44fbd900778ccd
Martin Øinæs Myrseth <myrseth@gmail.com>
2019-03-13 10:56:20 +0100

    Migrate 2018 day 1 to test spec

--- src/Aoc2018/Day1/Day1.hs
+++ src/Aoc2018/Day1/Day1.hs
@@ -14,14 +14,11 @@ changeList = do
   input <- readLines "./src/Aoc2018/Day1/input.txt"
   return $ mapMaybe parseShift input
 
-part2 :: [Int] -> Int
-part2 = go empty . scanl (+) 0 . cycle
+part1 :: IO Int
+part1 = sum <$> changeList
+
+part2 :: IO Int
+part2 = go empty . scanl (+) 0 . cycle <$> changeList
   where go set (f:fs) | f `member` set = f
                       | otherwise = go (f `insert` set) fs
         go _ _ = undefined
-
-puzzle :: IO ()
-puzzle = do
-  list <- changeList
-  expect "part 1: " 531 (sum list)
-  expect "part 2: " 76787 (part2 list)
--- test/Main.hs
+++ test/Main.hs
@@ -1,6 +1,12 @@
 module Main where
 
 import qualified Aoc2018.Day1.Day1 as Day1
+import qualified Aoc2018.Day10.Day10 as Day10
+import qualified Aoc2018.Day11.Day11 as Day11
+import qualified Aoc2018.Day12.Day12 as Day12
+import qualified Aoc2018.Day13.Day13 as Day13
+import qualified Aoc2018.Day14.Day14 as Day14
+import qualified Aoc2018.Day15.Day15 as Day15
 import qualified Aoc2018.Day2.Day2 as Day2
 import qualified Aoc2018.Day3.Day3 as Day3
 import qualified Aoc2018.Day4.Day4 as Day4
@@ -9,32 +15,31 @@ import qualified Aoc2018.Day6.Day6 as Day6
 import qualified Aoc2018.Day7.Day7 as Day7
 import qualified Aoc2018.Day8.Day8 as Day8
 import qualified Aoc2018.Day9.Day9 as Day9
-import qualified Aoc2018.Day10.Day10 as Day10
-import qualified Aoc2018.Day11.Day11 as Day11
-import qualified Aoc2018.Day12.Day12 as Day12
-import qualified Aoc2018.Day13.Day13 as Day13
-import qualified Aoc2018.Day14.Day14 as Day14
-import qualified Aoc2018.Day15.Day15 as Day15
+import           Test.Hspec
 
 main :: IO ()
-main = mapM_ printPuzzle $ zip ([1 ..] :: [Integer])
-  [ Day1.puzzle
-  , Day2.puzzle
-  , Day3.puzzle
-  , Day4.puzzle
-  , Day5.puzzle
-  , Day6.puzzle
-  , Day7.puzzle
-  , Day8.puzzle
-  , Day9.puzzle
-  , Day10.puzzle
-  , Day11.puzzle
-  , Day12.puzzle
-  , Day13.puzzle
-  , Day14.puzzle
-  , Day15.puzzle
-  ]
-  where
-    printPuzzle x = do
-      putStrLn $ show (fst x) <> "."
-      snd x
+main = hspec $ do
+  describe "Advent of Code 2018" $ do
+    describe "day1" $ do
+      it "solves part 1" $ do
+        part1 <- Day1.part1
+        part1 `shouldBe` 531
+
+      it "solves part 2" $ do
+        part2 <- Day1.part2
+        part2 `shouldBe` 76787
+
+    it "solves day 2"  Day2.puzzle
+    it "solves day 3"  Day3.puzzle
+    it "solves day 4"  Day4.puzzle
+    it "solves day 5"  Day5.puzzle
+    it "solves day 6"  Day6.puzzle
+    it "solves day 7"  Day7.puzzle
+    it "solves day 8"  Day8.puzzle
+    it "solves day 9"  Day9.puzzle
+    it "solves day 10" Day10.puzzle
+    it "solves day 11" Day11.puzzle
+    it "solves day 12" Day12.puzzle
+    it "solves day 13" Day13.puzzle
+    it "solves day 14" Day14.puzzle
+    xit "solves day 15" Day15.puzzle
