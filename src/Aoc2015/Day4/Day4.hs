710595e0dfa99ec6d0c98c0e57f283ebf4765327
Martin Øinæs Myrseth <myrseth@gmail.com>
2019-03-27 23:29:59 +0100

    2015: day4: More clean up

--- src/Aoc2015/Day4/Day4.hs
+++ src/Aoc2015/Day4/Day4.hs
@@ -17,7 +17,8 @@ md5list = map (id &&& compute) [1 ..]
 
 findMd5 :: Int -> IO Int
 findMd5 n = do
-  let result = fst <$> find (isPrefixOf (replicate n '0') . snd) md5list
+  let prefix = replicate n '0'
+      result = fst <$> find (isPrefixOf prefix . snd) md5list
   maybe (fail "md5 not found") pure result
 
 part1 :: IO Int
