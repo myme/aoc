6b4b0f3f1d0490560c29e434fc9bb94a50882c36
Martin Øinæs Myrseth <myrseth@gmail.com>
2021-12-12 23:41:08 +0100

    Move solutions into dirs based on language

--- rust/Aoc2021/src/main.rs
+++ rust/Aoc2021/src/main.rs
@@ -17,16 +17,23 @@ fn read_lines(fname: &str) -> Vec<String> {
     lines
 }
 
-fn run_day(day: u32, in_file: &str, func: fn(&Vec<String>) -> (i32, i32)) {
+fn run_day(
+    day: u32,
+    in_file: &str,
+    func: fn(&Vec<String>) -> (i32, i32),
+    answer: (i32, i32)
+) {
     let input = read_lines(in_file);
     let (part1, part2) = func(&input);
+    assert_eq!(part1, answer.0);
+    assert_eq!(part2, answer.1);
     println!("Day {}", day);
     println!("  Part 1: {}", part1);
     println!("  Part 2: {}", part2);
 }
 
 fn main() {
-    run_day(1, "./input/day1.txt", day1::day1);
-    run_day(2, "./input/day2.txt", day2::day2);
-    run_day(3, "./input/day3.txt", day3::day3);
+    run_day(1, "./input/day1.txt", day1::day1, (1832, 1858));
+    run_day(2, "./input/day2.txt", day2::day2, (2117664, 2073416724));
+    run_day(3, "./input/day3.txt", day3::day3, (4118544, 3832770));
 }
