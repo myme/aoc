5c3d8b6ca47915e2ce2cc2a1db88b48182fe2be8
Martin Øinæs Myrseth <myrseth@gmail.com>
2021-12-13 02:19:36 +0100

    2021: Day 4, part 2

--- rust/Aoc2021/src/day4.rs
+++ rust/Aoc2021/src/day4.rs
@@ -1,17 +1,20 @@
-type Board = [(i32, bool); 25];
+struct Board {
+    won: bool,
+    cells: [(i32, bool); 25],
+}
 
 fn parse_boards(lines: &[String]) -> Vec<Board> {
     let mut boards = vec!();
-    let mut board: Board = [(0, false); 25];
+    let mut board: Board = Board { won: false, cells: [(0, false); 25] };
 
     for (line_idx, line) in lines.iter().enumerate() {
         for (word_idx, word) in line.split(' ').filter(|w| !w.is_empty()).enumerate() {
             let idx = (line_idx % 5) * 5 + word_idx;
-            board[idx] = (word.parse().unwrap(), false);
+            board.cells[idx] = (word.parse().unwrap(), false);
 
             if idx == 24 {
                 boards.push(board);
-                board = [(0, false); 25];
+                board = Board { won: false, cells: [(0, false); 25] };
             }
         }
     }
@@ -22,7 +25,7 @@ fn parse_boards(lines: &[String]) -> Vec<Board> {
 fn check_win(board: &Board) -> bool {
     'rows: for line_idx in 0..5 {
         for word_idx in 0..5 {
-            if !board[line_idx * 5 + word_idx].1 {
+            if !board.cells[line_idx * 5 + word_idx].1 {
                 continue 'rows;
             }
         }
@@ -31,7 +34,7 @@ fn check_win(board: &Board) -> bool {
 
     'columns: for word_idx in 0..5 {
         for line_idx in 0..5 {
-            if !board[line_idx * 5 + word_idx].1 {
+            if !board.cells[line_idx * 5 + word_idx].1 {
                 continue 'columns;
             }
         }
@@ -44,8 +47,8 @@ fn check_win(board: &Board) -> bool {
 fn sum_board_unchecked(board: &Board) -> i32 {
     let mut sum = 0;
 
-    for (value, checked) in board {
-        if !*checked {
+    for (value, checked) in board.cells {
+        if !checked {
             sum += value;
         }
     }
@@ -53,17 +56,24 @@ fn sum_board_unchecked(board: &Board) -> i32 {
     sum
 }
 
-fn play_bingo(boards: &mut Vec<Board>, draws: &Vec<i32>) -> i32 {
+fn play_bingo(boards: &mut Vec<Board>, draws: &Vec<i32>, last_winner: bool) -> i32 {
+    let mut remain = boards.len();
+
     for draw in draws {
         for board in &mut *boards {
-            for cell in &mut *board {
-                if cell.0 == *draw {
-                    cell.1 = true;
+            if !board.won {
+                for cell in &mut board.cells {
+                    if cell.0 == *draw {
+                        cell.1 = true;
+                    }
                 }
-            }
-            if check_win(&board) {
-                println!("WIN! {}", draw);
-                return sum_board_unchecked(&board) * draw;
+                if check_win(&board) {
+                    board.won = true;
+                    if !last_winner || remain == 1 {
+                        return sum_board_unchecked(&board) * draw;
+                    }
+                    remain -= 1;
+                }
             }
         }
     }
@@ -73,7 +83,12 @@ fn play_bingo(boards: &mut Vec<Board>, draws: &Vec<i32>) -> i32 {
 
 pub fn day4(lines: &Vec<String>) -> (i32, i32) {
     let draws: Vec<i32> = lines[0].split(',').map(|v| v.parse().unwrap()).collect();
+
     let mut boards = parse_boards(&lines[1..]);
-    let part1 = play_bingo(&mut boards, &draws);
-    (part1, 0)
+    let part1 = play_bingo(&mut boards, &draws, false);
+
+    let mut boards = parse_boards(&lines[1..]);
+    let part2 = play_bingo(&mut boards, &draws, true);
+
+    (part1, part2)
 }
--- rust/Aoc2021/src/main.rs
+++ rust/Aoc2021/src/main.rs
@@ -45,5 +45,5 @@ fn main() {
     run_day(1, "./input/day1.txt", day1::day1, (1832, 1858));
     run_day(2, "./input/day2.txt", day2::day2, (2117664, 2073416724));
     run_day(3, "./input/day3.txt", day3::day3, (4118544, 3832770));
-    run_day(4, "./input/day4.txt", day4::day4, (27027, 0));
+    run_day(4, "./input/day4.txt", day4::day4, (27027, 36975));
 }
