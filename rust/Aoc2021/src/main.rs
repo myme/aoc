82c3c41a52213338b4ed2f35ce832a65a75d2a6b
Martin Øinæs Myrseth <myrseth@gmail.com>
2021-12-15 01:12:06 +0100

    2021: Refactor main

--- rust/Aoc2021/src/main.rs
+++ rust/Aoc2021/src/main.rs
@@ -47,49 +47,52 @@ fn run_day(day: u32, in_file: &str, func: Handler, answer: (i64, i64)) {
 
 fn main() {
     let with_small = false;
-    let days: Vec<u32> = if std::env::args().len() > 1 {
-        std::env::args().skip(1).map(|v| v.parse().unwrap()).collect()
-    } else {
-        Vec::from_iter(1..=24)
-    };
+    let filter_days: Vec<u32> = std::env::args()
+        .skip(1)
+        .map(|v| v.parse().unwrap())
+        .collect();
 
-    let solutions: Vec<(u32, Handler, Answer)> = vec!(
-        (1, day1::day1, Answer {
+    let solutions: Vec<(Handler, Answer)> = vec!(
+        (day1::day1, Answer {
             small: (7, 5),
             input: (1832, 1858),
         }),
-        (2, day2::day2, Answer {
+        (day2::day2, Answer {
             small: (150, 900),
             input: (2117664, 2073416724),
         }),
-        (3, day3::day3, Answer {
+        (day3::day3, Answer {
             small: (198, 230),
             input: (4118544, 3832770),
         }),
-        (4, day4::day4, Answer {
+        (day4::day4, Answer {
             small: (4512, 1924),
             input: (27027, 36975),
         }),
-        (5, day5::day5, Answer {
+        (day5::day5, Answer {
             small: (5, 12),
             input: (8350, 19374),
         }),
-        (6, day6::day6, Answer {
+        (day6::day6, Answer {
             small: (5934, 26984457539),
             input: (353274, 1609314870967),
         }),
-        (7, day7::day7, Answer {
+        (day7::day7, Answer {
             small: (0, 0),
             input: (0, 0),
         }),
     );
 
-    for (day, func, answer) in solutions {
+    for (day, (func, answer)) in solutions.iter().enumerate() {
+        let day: u32 = (day + 1).try_into().unwrap();
+        if !filter_days.is_empty() && !filter_days.contains(&day) {
+            continue;
+        }
+
         let input = if with_small { "small" } else { "input" };
         let answer = if with_small { answer.small } else { answer.input };
-        if days.contains(&day) {
-            let input = format!("./input/day{}-{}.txt", day, input);
-            run_day(day, &input, func, answer);
-        }
+        let input = format!("./input/day{}-{}.txt", day, input);
+
+        run_day(day, &input, *func, answer);
     }
 }
