8e1996596a872e43a2fd30a867a565bb83017e56
Martin Øinæs Myrseth <myrseth@gmail.com>
2021-12-22 00:43:49 +0100

    2021: Day 10, part 2

--- rust/Aoc2021/src/day10.rs
+++ rust/Aoc2021/src/day10.rs
@@ -30,7 +30,52 @@ fn find_corrupted_score(input: &String) -> Option<i64> {
     None
 }
 
+fn fix_incomplete_line(input: &String) -> Option<i64> {
+    let chunk_chars: HashMap<char, char> = HashMap::from([
+        ('(', ')'),
+        ('[', ']'),
+        ('{', '}'),
+        ('<', '>'),
+    ]);
+
+    let scoring_table: HashMap<char, i64> = HashMap::from([
+        (')', 1),
+        (']', 2),
+        ('}', 3),
+        ('>', 4),
+    ]);
+
+    let mut chunk_stack = VecDeque::new();
+
+    for chr in input.chars() {
+        if let Some(closing) = chunk_chars.get(&chr) {
+            chunk_stack.push_front(*closing);
+        } else if let Some(closing) = chunk_stack.pop_front() {
+            if chr != closing {
+                return None
+            }
+        }
+    }
+
+    if !chunk_stack.is_empty() {
+        let mut sum = 0_i64;
+
+        while let Some(closing) = chunk_stack.pop_front() {
+            sum = sum * 5 + scoring_table.get(&closing)?;
+        }
+
+        return Some(sum);
+    }
+
+    None
+}
+
 pub fn day10(lines: &Vec<String>) -> (i64, i64) {
     let part1 = lines.iter().map(find_corrupted_score).flatten().sum();
-    (part1, 0)
+
+    let mut fixed: Vec<i64> = lines.iter().filter_map(fix_incomplete_line).collect();
+    fixed.sort();
+    let part2 = fixed[fixed.len() / 2];
+
+    (part1, part2)
 }
