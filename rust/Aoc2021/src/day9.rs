8dae7b6c6f783ff8cdc1c85566f59dde9b8a527c
Martin Øinæs Myrseth <myrseth@gmail.com>
2021-12-21 15:51:59 +0100

    2021: Day 9, refactor pre part 2

--- rust/Aoc2021/src/day9.rs
+++ rust/Aoc2021/src/day9.rs
@@ -11,36 +11,48 @@ fn parse_grid(lines: &Vec<String>) -> Grid {
     grid
 }
 
-fn find_low_point_sum(lines: &Vec<String>) -> i64 {
-    let grid = parse_grid(lines);
-    let height = grid.len();
-    let width = grid[0].len();
+#[allow(dead_code)]
+struct Point {
+    row: usize,
+    col: usize,
+    height: u8,
+}
 
-    let mut sum = 0;
+fn find_low_points(grid: &Grid) -> Vec<Point> {
+    let mut points = Vec::new();
+    let grid_height = grid.len();
+    let grid_width = grid[0].len();
 
     for (row_idx, row) in grid.iter().enumerate() {
-        for (col_idx, depth) in row.iter().enumerate() {
-            if row_idx > 0 && depth >= &grid[row_idx - 1][col_idx] {
+        for (col_idx, height) in row.iter().enumerate() {
+            if row_idx > 0 && height >= &grid[row_idx - 1][col_idx] {
                 continue;
             }
-            if col_idx > 0 && depth >= &grid[row_idx][col_idx - 1] {
+            if col_idx > 0 && height >= &grid[row_idx][col_idx - 1] {
                 continue;
             }
-            if row_idx < height - 1 && depth >= &grid[row_idx + 1][col_idx] {
+            if row_idx < grid_height - 1 && height >= &grid[row_idx + 1][col_idx] {
                 continue;
             }
-            if col_idx < width - 1 && depth >= &grid[row_idx][col_idx + 1] {
+            if col_idx < grid_width - 1 && height >= &grid[row_idx][col_idx + 1] {
                 continue;
             }
-            sum += 1 + i64::from(*depth);
+            points.push(Point {
+                row: row_idx,
+                col: col_idx,
+                height: *height,
+            });
         }
     }
 
-    sum
+    points
 }
 
 pub fn day9(lines: &Vec<String>) -> (i64, i64) {
-    let part1 = find_low_point_sum(lines);
+    let grid = parse_grid(lines);
+    let low_points = find_low_points(&grid);
+
+    let part1 = low_points.iter().map(|v| i64::from(v.height) + 1).sum();
 
     (part1, 0)
 }
