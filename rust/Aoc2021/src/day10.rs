b1e81fc827f95cf327259513d60edd636281e48c
Martin Øinæs Myrseth <myrseth@gmail.com>
2021-12-22 00:24:23 +0100

    2021: Day 10, part 1

--- rust/Aoc2021/src/day10.rs
+++ rust/Aoc2021/src/day10.rs
@@ -1,3 +1,36 @@
-pub fn day10(_lines: &Vec<String>) -> (i64, i64) {
-    (0, 0)
+use std::collections::{HashMap, VecDeque};
+
+fn find_corrupted_score(input: &String) -> Option<i64> {
+    let chunk_chars: HashMap<char, char> = HashMap::from([
+        ('(', ')'),
+        ('[', ']'),
+        ('{', '}'),
+        ('<', '>'),
+    ]);
+
+    let scoring_table: HashMap<char, i64> = HashMap::from([
+        (')', 3),
+        (']', 57),
+        ('}', 1197),
+        ('>', 25137),
+    ]);
+
+    let mut chunk_stack = VecDeque::new();
+
+    for chr in input.chars() {
+        if let Some(closing) = chunk_chars.get(&chr) {
+            chunk_stack.push_front(*closing);
+        } else if let Some(closing) = chunk_stack.pop_front() {
+            if chr != closing {
+                return scoring_table.get(&chr).map(|v| *v);
+            }
+        }
+    }
+
+    None
+}
+
+pub fn day10(lines: &Vec<String>) -> (i64, i64) {
+    let part1 = lines.iter().map(find_corrupted_score).flatten().sum();
+    (part1, 0)
 }
