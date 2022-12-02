97002b019f29027a091723bc240bf7930427af00
Martin Myrseth <mm@myme.no>
2022-12-02 10:08:22 +0100

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+            rust-analyzer
+            rustc
+            rustfmt
+          ];
+        };
+
+      });
+}
