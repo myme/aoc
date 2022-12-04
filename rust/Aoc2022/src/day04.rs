9159d5208720b678b0e3a8ddb43ac09443c6ca4f
Martin Myrseth <mm@myme.no>
2022-12-04 20:59:00 +0100

    2022: Day 4 WIP

--- rust/Aoc2022/src/day04.rs
+++ rust/Aoc2022/src/day04.rs
@@ -1,7 +1,18 @@
-fn part1(_input: &str) -> u64 {
-    0
+fn part1(input: &str) -> Option<u64> {
+    let mut count = 0u64;
+
+    for line in input.lines() {
+        // let mut splits = line.split(",");
+        // splits.next()?.split("-");
+        // splits.next()?;
+        match line[..] {
+            ["a"] => 0,
+        };
+    }
+
+    Some(count)
 }
 
 pub fn day4(input: &str) -> (u64, u64) {
-    (part1(input), 0)
+    (part1(input).unwrap_or(0), 0)
 }
