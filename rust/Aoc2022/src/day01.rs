26de580c5cfde4a3c83fd9f30a5fd7a15afbe307
Martin Myrseth <mm@myme.no>
2022-12-04 10:05:08 +0100

    2022: Day 1, fix bug
    Need to take the sum of the last lines/elf, assuming there's no trailing
    newline.

--- rust/Aoc2022/src/day01.rs
+++ rust/Aoc2022/src/day01.rs
@@ -10,8 +10,8 @@ pub fn day1(input: &str) -> (i64, i64) {
     let mut sums = Vec::<i64>::new();
     let mut sum: i64 = 0;
 
-    for line in input.lines() {
-        if !line.trim().is_empty() {
+    for line in input.lines().map(|l| l.trim()) {
+        if !line.is_empty() {
             sum += line.parse::<i64>().unwrap();
         } else {
             sums.push(sum);
@@ -19,6 +19,7 @@ pub fn day1(input: &str) -> (i64, i64) {
         }
     }
 
+    sums.push(sum);
     sums.sort_by(|a, b| b.cmp(a));
 
     (part1(&sums), part2(&sums))
